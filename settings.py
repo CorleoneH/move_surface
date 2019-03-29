64968922970472dab9d2741d57c4842c78aa9b56
CorleoHuang <Longhuang0720@gmail.com>
2019-03-29 11:44:17 +0800

    Update settings.py
    correction of an error in the comment.

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ class  Setting(object):
 
     def __init__(self):
         """screen settings"""
-        self.screen_size = (640, 480)
+        self.screen_size = (640, 480)       #screen size
         self.bg_color = (255, 255, 255)     #white
 
         """surface settings"""
         self.sur_color = (0, 0, 0)      #black
-        self.sur_size = (20, 20)        #surface sur_size
+        self.sur_size = (20, 20)        #surface size
         self.sur_topleft = (100, 100)
         self.sur_speed = 10     #surface moving speed
 
